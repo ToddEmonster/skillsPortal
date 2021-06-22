b096f45d2b730c6b70b310de6426c324a21096b0
Todd Raharivelo <s.raharivelo@outlook.fr>
2021-06-22 21:12:29 +0200

    feat: add profile form

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Profile;
+use App\Form\ProfileType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,16 +19,46 @@ class ProfileController extends AbstractController
     }
 
     /**
-     * @Route("/profile", name="profile")
+     * @Route("/profile/all", name="all_profiles")
      */
     public function index(): Response
     {
-        //$profile = $this->entityManager->getRepository(Profile::class)->findAll();
-
-        //dd($profile);
+        $profile = $this->entityManager->getRepository(Profile::class)->findAll();
 
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
         ]);
+    }
+
+    /**
+     * @Route(
+     *     "/profile/{id}",
+     *     name="my_profile",
+     *     methods={"GET"},
+     *     requirements={"id"="\d+"}
+     * )
+     */
+    public function read(int $id = 1): Response
+    {
+        $profile = $this->entityManager->getRepository(Profile::class)->findOneByUser($id);
+
+        return $this->render('profile/my_profile.html.twig', [
+            'controller_name' => 'ProfileController',
+            'profile_name' => $profile->getFirstName() . ' ' . $profile->getLastName()
+        ]);
+    }
+
+    /**
+     * @Route("/profile/new", name="new_profile")
+     */
+    public function new(Request $request): Response
+    {
+        $profile = new Profile();
+        $form = $this->createForm(ProfileType::class, $profile);
+
+        return $this->render(
+            'profile/new.html.twig',
+            ['form' => $form->createView()]
+        );
     }
 }
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -18,13 +18,9 @@ class ProfileType extends AbstractType
             ->add('position')
             ->add('email')
             ->add('tel')
-            ->add('creationDate')
-            ->add('lastEditDate')
             ->add('address')
             ->add('postalCode')
             ->add('city')
-            ->add('apsideBirthday')
-            ->add('user')
         ;
     }
 
